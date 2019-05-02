be49d95fdc05f1a5556adc9b134cb38bc6b63945
Erick Kinnee <erick@kinnee.net>
2019-05-02 15:53:47 -0500

    Clean up and formatting.

--- CopySerial/CopySerial.ino
+++ CopySerial/CopySerial.ino
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-#include <SoftwareSerial.h>
-
-SoftwareSerial ZERO(4, 7); // RX, TX
-
-void software_Reset()
-{
-    asm volatile ("  jmp 0");  
-} 
-
-void setup() {
-  Serial.begin(38400);
-  while (!Serial)
-  {
-  }
-  ZERO.begin(38400);
-  while (!ZERO)
-  {
-  }
-}
-
-void loop() {
-  if (Serial.available())
-  {
-    ZERO.write(Serial.read());
-  }
-  if (ZERO.available())
-  {
-    Serial.write(ZERO.read());
-  }
-}
-=======
 #include <SoftwareSerial.h>
 
 SoftwareSerial ZERO(4, 7); // RX, TX
@@ -57,4 +25,3 @@ void loop() {
         Serial.write(ZERO.read());
     }
 }
->>>>>>> 3b8f96c62c10c094f8f550a12de9f957d2514265
